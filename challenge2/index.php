946d23054d0c19c7b5721c3725d02d07832fad3b
Aaron Weidele <aweidele@gmail.com>
2021-08-16 20:20:27 -0400

    Challege 2

---
+++ challenge2/index.php
@@ -0,0 +1,101 @@
+<?php
+  function isPermutation($str1,$str2) {
+    $chars1 = str_split($str1);
+    $chars2 = str_split($str2);
+    sort($chars1);
+    sort($chars2);
+
+    foreach($chars1 as $i => $char) {
+      if($char != $chars2[$i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Elevetated Third Code Challenges</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Open+Sans:wght@300;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+  <header>
+    <h1>String 1 is permutation of String 2</h1>
+    <code>
+<pre>
+function isPermutation($str1,$str2) {
+  $chars1 = str_split($str1);
+  $chars2 = str_split($str2);
+  sort($chars1);
+  sort($chars2);
+
+  foreach($chars1 as $i =&gt; $char) {
+    if($char != $chars2[$i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+</pre>
+    </code>
+  </header>
+  <main class="main">
+    <?php
+      $str1 = "newyorktimes";
+      $str2 = "monkeyswrite";
+    ?>
+    <section>
+      <h2>Sample 1</h2>
+      <p><strong>String 1: <?php echo $str1; ?></strong></p>
+      <p><strong>String 2: <?php echo $str2; ?></strong></p>
+      <p><strong>Result:</strong> <?php echo isPermutation($str1,$str2) ? "True" : "False"; ?></p>
+    </section>
+    <?php
+      $str1 = "abcdefg";
+      $str2 = "agbfced";
+    ?>
+    <section>
+      <h2>Sample 2</h2>
+      <p><strong>String 1: <?php echo $str1; ?></strong></p>
+      <p><strong>String 2: <?php echo $str2; ?></strong></p>
+      <p><strong>Result:</strong> <?php echo isPermutation($str1,$str2) ? "True" : "False"; ?></p>
+    </section>
+    <?php
+      $str1 = "abcdefg";
+      $str2 = "agbffed";
+    ?>
+    <section>
+      <h2>Sample 2</h2>
+      <p><strong>String 1: <?php echo $str1; ?></strong></p>
+      <p><strong>String 2: <?php echo $str2; ?></strong></p>
+      <p><strong>Result:</strong> <?php echo isPermutation($str1,$str2) ? "True" : "False"; ?></p>
+    </section>
+    <?php
+      $str1 = "dog";
+      $str2 = "cat";
+    ?>
+    <section>
+      <h2>Sample 2</h2>
+      <p><strong>String 1: <?php echo $str1; ?></strong></p>
+      <p><strong>String 2: <?php echo $str2; ?></strong></p>
+      <p><strong>Result:</strong> <?php echo isPermutation($str1,$str2) ? "True" : "False"; ?></p>
+    </section>
+    <?php
+      $str1 = "live";
+      $str2 = "evil";
+    ?>
+    <section>
+      <h2>Sample 2</h2>
+      <p><strong>String 1: <?php echo $str1; ?></strong></p>
+      <p><strong>String 2: <?php echo $str2; ?></strong></p>
+      <p><strong>Result:</strong> <?php echo isPermutation($str1,$str2) ? "True" : "False"; ?></p>
+    </section>
+  </main>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,31 @@
+<?php
+  function allUnique($str) {
+    $chars = str_split($str);
+    sort($chars);
+    for($i=1;$i<sizeof($chars);$i++) {
+      if($chars[$i] == $chars[$i-1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
   <title>Elevetated Third Code Challenges</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Open+Sans:wght@300;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-  Hi.
+  <header>
+    <h1>Code Challenges</h1>
+  </header>
+  <main class="main">
+    <p><a href="challenge1">Challenge 1</a></p>
+    <p><a href="challenge2">Challenge 2</a></p>
+  </main>
 </body>
 </html>
